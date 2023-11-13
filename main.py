8e39456d46c83af63986d9db224188f0c6a7878a
aprilhulu <aisai051reika@gmail.com>
2023-11-13 15:22:00 +0900

    1.3

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import random
-
-#import subprocess
-#import ffmpeg
-#from voice_generator import creat_WAV
-
-
 import discord
 from discord import app_commands
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
-#voice_client = None
-#bot = commands.Bot(intents=discord.Intents.all(),command_prefix="!")
-
 OM = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶']
+tokumei = "匿名ちゃねら"
 
 #起動確認
 @client.event
@@ -29,37 +21,26 @@ async def on_ready():
 #ちょっとテスト用
 @client.event
 async def on_message(message):
- if message.author == client.user:
-  return
  if message.content == "テスト":
   await message.reply("成功")
+#DMを匿名化して匿名ちゃねらで叫ぶ
+ if type(message.channel) == discord.DMChannel : 
+#dmかどうか
+  if message.author.bot: #BOTの場合は何もせず終了
+   return
+        #メッセージ送信部
+  for channel in client.get_all_channels(): #BOTが所属する全てのチャンネルをループ
+   if channel.name == tokumei: #匿名チャンネルが見つかったとき                
+    embed=discord.Embed(description=message.content, color=0x9B95C9) 
+    #埋め込みの説明に、メッセージを挿入し、埋め込みのカラーを紫`#9B95C9`に設定
+    if message.attachments != []: #添付ファイルが存在するとき
+     embed.set_image(url=message.attachments[0].url)
+    await channel.send(embed=embed) #メッセージを送信
+  await message.add_reaction('✅') #リアクションを送信
+
 #おみくじコマンド
 @tree.command(name="omikuji", description="おみくじを引きます")
 async def omikuji(interaction: discord.Interaction):
  await interaction.response.send_message(f'今日のの運勢は"{OM[random.randrange(len(OM))]}"です')
-#封印されし読み上げ機能
-#@tree.command(name="join",description="ボイスチャンネルに参加")
-#async def join(ctx):
- #voicechannelを取得
- #vc = ctx.author.voice.channel
- #voicechannelに接続
- #await vc.connect()
-#@tree.command(name="disc",description="ボイスチャンネルから退出")
-#async def bye(ctx):
-#切断
-#await ctx.voice_client.disconnect()
-#@client.event
-#async def on_message(message):
- #if message.guild.voice_client:
-  #print(message.content)
-  #creat_WAV(message.content)
-  #source = discord.FFmpegPCMAudio("output.wav")
-  #message.guild.#voice_client.play(source)
-#else:
- #pass
 
-#@tree.command(name="gup", description="叫びます。グッピー")
-#async def gup(interaction: discord.Interaction):
- # msg = "グッピー"
-  #await interaction.response.send_message(msg)
 client.run(os.getenv("DISCORD_TOKEN"))
